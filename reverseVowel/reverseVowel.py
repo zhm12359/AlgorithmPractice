e968669682605cb8e63537550bcc79d72b051fba
hanming zeng <hz926@nyu.edu>
2016-10-03 10:47:05 -0400

    reverseVowel

---
+++ reverseVowel/reverseVowel.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        sl = list(s)
+
+        i = 0
+        j = len(s) - 1
+
+        while( i < j ):
+
+            if sl[i] in 'aeiouAEIOU' and sl[j] in 'aeiouAEIOU':
+
+                sl[i],sl[j] = sl[j],sl[i]
+
+                i = i +1
+                j = j -1
+            elif sl[i] in 'aeiouAEIOU':
+                j = j-1
+            else:
+                i = i+1
+
+        s = "".join(sl)
+        return s
+        
